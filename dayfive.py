05bc4ee7bf6ea446e8eca1d06d1fc18339deced9
root <deadman94-cyber>
2020-07-04 03:03:03 +0530

    5th day

---
+++ dayfive.py
@@ -0,0 +1,13 @@
+a=input("input a number =")
+b=input("input another number = ")
+
+def car(a,b):
+    
+    if a < b:
+        print(a)
+def cdr(a,b):
+    if a <b:
+        print(b)
+
+car(a,b)
+cdr(a,b)
